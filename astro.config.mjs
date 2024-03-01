0a6abe98eb5f9f4baf37bd0ddd9b8ee74590cf18
Danielru <danielru@MacBook-Pro-de-Daniel.local>
2024-03-01 14:18:50 -0500

    solucion robots y mejora recomendaciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercelStatic from '@astrojs/vercel/static';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config 
 export default defineConfig({
     site: 'https://www.proyexapp.com',
     integrations: [sitemap()],
+    output: 'static',
+    adapter: vercelStatic(),
   });
